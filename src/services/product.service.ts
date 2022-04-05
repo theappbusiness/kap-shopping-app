15300ab3d64e2039d6294fc1a9f04eca15df57ce
Erica Carvalho <erica.carvalho@kinandcarta.com>
2022-04-05 14:45:44 +0100

    Remove throw

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -3,19 +3,11 @@ import { Product } from "../types/product";
 import { API_GET_PRODUCT, API_GET_PRODUCTS } from "../constants/api";
 
 export const getProductData = async (id: string): Promise<Product> => {
-  try {
-    const { data } = await axios.get(API_GET_PRODUCT(id));
-    return data;
-  } catch (error) {
-    throw new Error((error as Error).message);
-  }
+  const { data } = await axios.get(API_GET_PRODUCT(id));
+  return data;
 };
 
 export const getProductsData = async (): Promise<Product[]> => {
-  try {
-    const { data } = await axios.get(API_GET_PRODUCTS);
-    return data;
-  } catch (error) {
-    throw new Error((error as Error).message);
-  }
+  const { data } = await axios.get(API_GET_PRODUCTS);
+  return data;
 };
