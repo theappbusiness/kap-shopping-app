7004167845be3a3bffd15e21475f71832072f8dc
Erica Carvalho <erica.carvalho@kinandcarta.com>
2022-04-07 16:31:31 +0100

    Add translation example

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import styled from "styled-components";
+import { useTranslation } from "react-i18next";
+import "./translations/i18n";
 import GlobalStyle from "./GlobalStyle";
 import ProductTiles from "./components/product-tiles/ProductTiles";
 
 const AppContainer = styled.div`
   max-width: 120rem;
   margin: auto;
+
+  & .title {
+    font-size: 3rem;
+    text-align: center;
+  }
 `;
 
 function App() {
+  const { t } = useTranslation();
+
   return (
     <>
       <GlobalStyle />
       <AppContainer>
+        <h1 className="title">{t("hello")}</h1>
         <ProductTiles />
       </AppContainer>
     </>
