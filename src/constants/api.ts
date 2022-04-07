10b505d15317267e965592134ef752c9f9504e5f
Erica Carvalho <erica.carvalho@kinandcarta.com>
2022-04-07 16:43:01 +0100

    Update api url

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,4 @@
-const HOST = "http://localhost:5000";
+const HOST = "http://54.153.84.234:3000";
 export const API = `${HOST}/api/v1`;
 export const API_GET_PRODUCTS = `${API}/products`;
 export const API_GET_PRODUCT = (id: string) => `${API_GET_PRODUCTS}/${id}`;
