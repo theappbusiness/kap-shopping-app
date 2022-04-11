b217e9300785bee47a4b41aa6e882d9d18de5478
Erica Carvalho <94441307+ericamcarvalho@users.noreply.github.com>
2022-04-11 15:52:40 +0100

    Update API url

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,4 @@
-const HOST = "http://54.153.84.234:3000";
+const HOST = "http://ec2-54-153-84-234.us-west-1.compute.amazonaws.com/api/v1/products";
 export const API = `${HOST}/api/v1`;
 export const API_GET_PRODUCTS = `${API}/products`;
 export const API_GET_PRODUCT = (id: string) => `${API_GET_PRODUCTS}/${id}`;
