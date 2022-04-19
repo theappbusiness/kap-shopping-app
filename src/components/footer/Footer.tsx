0c2c94f986cccea93b623baa7cf143f13b2168da
Isabel Pinder <isabel.pinder@kinandcarta.com>
2022-04-19 16:16:40 +0100

    add styling to Footer component

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,33 @@
 import styled from 'styled-components';
 
+const FooterContainer = styled.footer`
+  display: flex;
+  justify-content: center;
+  width: 100vw;
+  height: auto;
+  background-color: ${({ theme }) => theme.colors.light};
+  padding: ${({ theme }) => theme.sizes.spacing.sm};
+
+  & i {
+    font-size: ${({ theme }) => theme.sizes.fonts.xl};
+  }
+
+  & .footer-i-middle {
+    margin-left: ${({ theme }) => theme.sizes.spacing.sm};
+    margin-right: ${({ theme }) => theme.sizes.spacing.sm};
+  }
+`;
+
 const Footer = () => {
   return (
-    <footer data-testid="footer">
+    <FooterContainer data-testid="footer">
       <i className="fa-brands fa-twitter" data-testid="social-media"></i>
-      <i className="fa-brands fa-facebook" data-testid="social-media"></i>
+      <i
+        className="fa-brands fa-facebook footer-i-middle"
+        data-testid="social-media"
+      ></i>
       <i className="fa-brands fa-instagram" data-testid="social-media"></i>
-    </footer>
+    </FooterContainer>
   );
 };
 
