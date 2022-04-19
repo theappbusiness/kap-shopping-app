d6f4190fd5bb127bb5d5cfe2aecc745b7c9eeb33
KStonehouse2001 <khalidstonehouse12@outlook.com>
2022-04-19 16:33:51 +0100

    added search icon with styling & background theme changed

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const HeaderContainer = styled.div`
-  background-color: #d6d6d6;
+  background-color: #e4e4ee;
   display: flex;
   justify-content: center;
   padding: 10px;
@@ -16,12 +16,21 @@ const HeaderContainer = styled.div`
   .fa-bars:hover {
     color: #302f2f;
   }
+  .fa-magnifying-glass {
+    position: absolute;
+    right: 2%;
+    cursor: pointer;
+  }
+  .fa-magnifying-glass:hover {
+    color: #302f2f;
+  }
 `;
 const Header = () => {
   return (
     <HeaderContainer data-testid="header">
       <h1>Shoply</h1>
       <i className="fa-solid fa-bars fa-2x" data-testid="hamburger-icon"></i>
+      <i className="fa-solid fa-magnifying-glass fa-2x"></i>
     </HeaderContainer>
   );
 };
