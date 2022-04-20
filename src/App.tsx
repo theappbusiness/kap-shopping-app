fbd83611f250a9e58f036974533c7c97e52ff25d
Isabel Pinder <isabel.pinder@kinandcarta.com>
2022-04-20 10:00:12 +0100

    add container with flex to app to fix footer to bottom of page in desktop view

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import ProductTiles from './components/product-tiles/ProductTiles';
 import Footer from './components/footer/Footer';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
 
 function App() {
   return (
-    <>
+    <Container>
       <ProductTiles />
       <Footer />
-    </>
+    </Container>
   );
 }
 
