97d2a8b659b09d4133da3901490c08af1b594cfa
Khalid Stonehouse <khalidstonehouse12@outlook.com>
2022-04-20 09:46:51 +0100

    Revert "Header"

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import Header from './components/header/Header';
 import ProductTiles from './components/product-tiles/ProductTiles';
 import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <>
-      <Header>
       <ProductTiles />
       <Footer />
     </>
