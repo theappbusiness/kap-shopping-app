138377dc5f715416a1367a7726d423aa9aee6276
KStonehouse2001 <khalidstonehouse12@outlook.com>
2022-04-21 16:57:38 +0100

    Added handleClick test

--- src/components/general/button/Button.test.tsx
+++ src/components/general/button/Button.test.tsx
@@ -1,28 +1,35 @@
-import { render, screen } from '../../../test-utils';
+import { render, screen,fireEvent } from '../../../test-utils';
 import { Button } from './index';
 
 describe('Button renders', () => {
   test('Button renders correct text when given text as props', () => {
-    render(<Button text="given text" />);
+    render(<Button handleClick={() =>{}} text="given text" />);
     const button = screen.getByRole('button', { name: 'given text' });
     expect(button).toBeInTheDocument();
   });
 
   test('Button renders with primary color palette when passed no color props', () => {
-    render(<Button text="given text" />);
+    render(<Button handleClick={() =>{}} text="given text" />);
     const button = screen.getByRole('button', { name: 'given text' });
-
     expect(button).toHaveStyle(`background-color: #18181a`);
     expect(button).toHaveStyle(`border-color: #e4e4ee`);
     expect(button).toHaveStyle(`color: #e4e4ee`);
   });
 
   test('Button renders with danger color palette when passed color prop of "danger"', () => {
-    render(<Button text="given text" color="danger" />);
+    render(<Button handleClick={() =>{}} text="given text" color="danger" />);
     const button = screen.getByRole('button', { name: 'given text' });
-
     expect(button).toHaveStyle(`background-color: #e60927`);
     expect(button).toHaveStyle(`border-color: #e4e4ee`);
     expect(button).toHaveStyle(`color: #e4e4ee`);
   });
+
+  test('Button handleClick function is called once upon button being clicked ', () => {
+    const myMockFunction = jest.fn()
+    render(<Button handleClick={myMockFunction} text="given text" color="danger" />);
+    const button = screen.getByRole('button', { name: 'given text' });
+    fireEvent.click(button)
+    expect(myMockFunction).toHaveBeenCalledTimes(1)
+  });
+  
 });
