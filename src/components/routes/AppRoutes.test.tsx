2ec8b201f1c3919a3aab14ce6a9125cadb21a523
Isabel Pinder <isabel.pinder@kinandcarta.com>
2022-04-21 11:30:49 +0100

    create AppRoutes component and test each route renders

--- src/components/routes/AppRoutes.test.tsx
+++ src/components/routes/AppRoutes.test.tsx
@@ -1,8 +1,41 @@
+import { MemoryRouter } from 'react-router-dom';
 import { render, screen } from '../../test-utils';
 import AppRoutes from './AppRoutes';
-AppRoutes
+
 describe('React Router', () => {
-    it('Renders Products Page', () => {
-        
-    });
+  it('Renders Products Page', () => {
+    render(
+      <MemoryRouter initialEntries={['/products']}>
+        <AppRoutes />
+      </MemoryRouter>
+    );
+    expect(screen.getByText('Products Page')).toBeInTheDocument();
+  });
+
+  it('Renders Product Details Page', () => {
+    render(
+      <MemoryRouter initialEntries={['/products/1']}>
+        <AppRoutes />
+      </MemoryRouter>
+    );
+    expect(screen.getByText('Product Details Page')).toBeInTheDocument();
+  });
+
+  it('Renders Cart Page', () => {
+    render(
+      <MemoryRouter initialEntries={['/cart']}>
+        <AppRoutes />
+      </MemoryRouter>
+    );
+    expect(screen.getByText('Cart Page')).toBeInTheDocument();
+  });
+
+  it('Renders Payment', () => {
+    render(
+      <MemoryRouter initialEntries={['/payment']}>
+        <AppRoutes />
+      </MemoryRouter>
+    );
+    expect(screen.getByText('Payment Page')).toBeInTheDocument();
+  });
 });
--- src/components/routes/AppRoutes.tsx
+++ src/components/routes/AppRoutes.tsx
@@ -1,26 +1,20 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import CartPage from '../../pages/cart'
-import HomePage from '../../pages/home'
-import PaymentPage from '../../pages/payment'
-import ProductDetailsPage from '../../pages/productDetails'
-import ProductsPage from '../../pages/products'
-
+import { Routes, Route } from 'react-router-dom';
+import CartPage from '../../pages/cart';
+import HomePage from '../../pages/home';
+import PaymentPage from '../../pages/payment';
+import ProductDetailsPage from '../../pages/productDetails';
+import ProductsPage from '../../pages/products';
 
 const AppRoutes = () => {
-return (
-    <BrowserRouter>
+  return (
     <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/products/:id" element={<ProductDetailsPage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/payment" element={<PaymentPage />} />
-      </Routes>
-    </BrowserRouter>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/products/:id" element={<ProductDetailsPage />} />
+      <Route path="/cart" element={<CartPage />} />
+      <Route path="/payment" element={<PaymentPage />} />
+    </Routes>
+  );
+};
 
-    
-      )
-}
-
-
-export default AppRoutes
+export default AppRoutes;
