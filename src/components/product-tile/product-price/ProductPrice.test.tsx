36e8503cb0c7f49c35b28389461193bf33c82853
Isabel Pinder <isabel.pinder@kinandcarta.com>
2022-04-26 16:06:45 +0100

    test ProductPrice component renders £ or € based on locale

---
+++ src/components/product-tile/product-price/ProductPrice.test.tsx
@@ -0,0 +1,51 @@
+import { render, screen } from '../../../test-utils';
+import { Product } from '../../../types/product';
+import { ProductPrice } from './ProductPrice';
+const productDataMock: Product = {
+  id: '#1',
+  name: 'Some random product',
+  category: {
+    name: 'General',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
+  },
+  price: 17999.9,
+  stock: 5,
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
+};
+
+test('when browser locale set to England, price is formatted to £', () => {
+  const formattedPrice = productDataMock.price.toLocaleString('en-GB', {
+    style: 'currency',
+    currency: 'GBP',
+    minimumFractionDigits: 2,
+  });
+  render(<ProductPrice price={productDataMock.price} locale="en" />);
+  const productPrice = screen.getByText(formattedPrice);
+  expect(productPrice).toBeInTheDocument();
+});
+
+test('when browser locale set to Portuguese, price is formatted to €', () => {
+  const formattedPrice = productDataMock.price.toLocaleString('pt', {
+    style: 'currency',
+    currency: 'EUR',
+    minimumFractionDigits: 2,
+  });
+
+  render(<ProductPrice price={productDataMock.price} locale="pt" />);
+  const productPrice = screen.getByText(formattedPrice.normalize('NFKC'));
+  expect(productPrice).toBeInTheDocument();
+});
+
+test('when browser locale set to French, price is formatted to €', () => {
+  const formattedPrice = productDataMock.price.toLocaleString('fr', {
+    style: 'currency',
+    currency: 'EUR',
+    minimumFractionDigits: 2,
+  });
+
+  render(<ProductPrice price={productDataMock.price} locale="fr" />);
+  const productPrice = screen.getByText(formattedPrice.normalize('NFKC'));
+  expect(productPrice).toBeInTheDocument();
+});
