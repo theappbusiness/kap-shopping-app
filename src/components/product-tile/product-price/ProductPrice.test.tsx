466dd860e3d06c09b653052d8102bc563ac67410
Isabel Pinder <isabel.pinder@kinandcarta.com>
2022-04-26 16:28:59 +0100

    correct typo

--- src/components/product-tile/product-price/ProductPrice.test.tsx
+++ src/components/product-tile/product-price/ProductPrice.test.tsx
@@ -15,7 +15,7 @@ const productDataMock: Product = {
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
 };
 
-test('when browser locale set to England, price is formatted to £', () => {
+test('when locale set to British English, price is formatted to £', () => {
   const formattedPrice = productDataMock.price.toLocaleString('en-GB', {
     style: 'currency',
     currency: 'GBP',
@@ -26,7 +26,7 @@ test('when browser locale set to England, price is formatted to £', () => {
   expect(productPrice).toBeInTheDocument();
 });
 
-test('when browser locale set to Portuguese, price is formatted to €', () => {
+test('when locale set to Portuguese, price is formatted to €', () => {
   const formattedPrice = productDataMock.price.toLocaleString('pt', {
     style: 'currency',
     currency: 'EUR',
@@ -38,7 +38,7 @@ test('when browser locale set to Portuguese, price is formatted to €', () => {
   expect(productPrice).toBeInTheDocument();
 });
 
-test('when browser locale set to French, price is formatted to €', () => {
+test('when locale set to French, price is formatted to €', () => {
   const formattedPrice = productDataMock.price.toLocaleString('fr', {
     style: 'currency',
     currency: 'EUR',
