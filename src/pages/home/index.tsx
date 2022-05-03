9d3081a1695e2e8899ef67d030f55f54e6baeb89
KStonehouse2001 <khalidstonehouse12@outlook.com>
2022-05-03 11:22:04 +0100

    created hero component with image carousel

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,10 @@
 import { ProductTiles } from '../../components/product-tiles/ProductTiles';
-
+import { Hero } from '../../components/hero/index';
 export const HomePage: React.FC = () => {
-  return <ProductTiles />;
+  return (
+    <>
+      <Hero />
+      <ProductTiles />
+    </>
+  );
 };
