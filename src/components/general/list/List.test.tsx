1f83a6e50ec7351da660a10d9af067aeea1a7386
Isabel Pinder <isabel.pinder@kinandcarta.com>
2022-05-04 16:41:51 +0100

    use describe and it syntax in tests for consistency. Change screen methods to get when expecting a non empty array

--- src/components/general/list/List.test.tsx
+++ src/components/general/list/List.test.tsx
@@ -1,23 +1,27 @@
 import { render, screen } from '../../../test-utils';
 import { List } from '.';
 
-test('renders 0 list items when passed an empty array', () => {
-  render(<List items={[]} />);
+describe('List', () => {
+  it('renders 0 list items when passed an empty array', () => {
+    render(<List items={[]} />);
 
-  const listItems = screen.queryAllByRole('listitem');
-  expect(listItems).toEqual([]);
-});
+    const listItems = screen.queryAllByRole('listitem');
+    expect(listItems).toEqual([]);
+  });
 
-test('renders 1 list items when passed an array with 1 string element', () => {
-  render(<List items={['test-string']} />);
+  it('renders 1 list items when passed an array with 1 string element', () => {
+    render(<List items={['test-string']} />);
 
-  const listItems = screen.queryAllByRole('listitem');
-  expect(listItems).toHaveLength(1);
-});
+    const listItems = screen.getAllByRole('listitem');
+    expect(listItems).toHaveLength(1);
+  });
 
-test('renders 3 list items when passed an array with 1 string element', () => {
-  render(<List items={['test-string 1', 'test-string 2', 'test-string 3']} />);
+  it('renders 3 list items when passed an array with 1 string element', () => {
+    render(
+      <List items={['test-string 1', 'test-string 2', 'test-string 3']} />
+    );
 
-  const listItems = screen.queryAllByRole('listitem');
-  expect(listItems).toHaveLength(3);
+    const listItems = screen.getAllByRole('listitem');
+    expect(listItems).toHaveLength(3);
+  });
 });
