4d5abbcf1eeea04a91a99b07d9d24175aab34d08
Isabel Pinder <isabel.pinder@kinandcarta.com>
2022-05-04 17:12:51 +0100

    test list is in the document, even if no list items are rendered

--- src/components/general/list/List.test.tsx
+++ src/components/general/list/List.test.tsx
@@ -5,12 +5,18 @@ describe('List', () => {
   it('renders 0 list items when passed an empty array', () => {
     render(<List items={[]} />);
 
+    const list = screen.getByRole('list');
+    expect(list).toBeInTheDocument();
+
     const listItems = screen.queryAllByRole('listitem');
     expect(listItems).toEqual([]);
   });
 
   it('renders 1 list items when passed an array with 1 string element', () => {
     render(<List items={['test-string']} />);
+
+    const list = screen.getByRole('list');
+    expect(list).toBeInTheDocument();
 
     const listItems = screen.getAllByRole('listitem');
     expect(listItems).toHaveLength(1);
@@ -20,6 +26,9 @@ describe('List', () => {
     render(
       <List items={['test-string 1', 'test-string 2', 'test-string 3']} />
     );
+
+    const list = screen.getByRole('list');
+    expect(list).toBeInTheDocument();
 
     const listItems = screen.getAllByRole('listitem');
     expect(listItems).toHaveLength(3);
