055374e154f23bea0ab30c7270296c46e1c5f2e1
Isabel Pinder <isabel.pinder@kinandcarta.com>
2022-05-04 12:30:00 +0100

    test for component rendered with and without a placeholder

--- src/components/general/input/Input.test.tsx
+++ src/components/general/input/Input.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '../../../test-utils';
 import { Input } from '.';
 
 describe('Single line input component', () => {
-  it('renders a text input component that accepts an onChange props ', async () => {
+  it('renders a text input component that accepts an onChange props when no placeholder prop is passed', async () => {
     const user = userEvent.setup();
     const onChangeMock = jest.fn();
     render(<Input onChange={onChangeMock} />);
@@ -13,6 +13,19 @@ describe('Single line input component', () => {
 
     await user.type(input, 'foo');
     expect(input).toHaveValue('foo');
+    expect(onChangeMock).toBeCalledTimes(3);
+  });
+
+  it('renders a text input component that accepts an onChange props when a placeholder prop is passed', async () => {
+    const user = userEvent.setup();
+    const onChangeMock = jest.fn();
+    render(<Input onChange={onChangeMock} placeholder="given-placeholder" />);
+    const inputWithPlaceholder =
+      screen.getByPlaceholderText('given-placeholder');
+    expect(inputWithPlaceholder).toBeInTheDocument();
+
+    await user.type(inputWithPlaceholder, 'foo');
+    expect(inputWithPlaceholder).toHaveValue('foo');
     expect(onChangeMock).toBeCalledTimes(3);
   });
 });
