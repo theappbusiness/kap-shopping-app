c43ae604c8221587b811ac3179b48eb2b3fe2a2b
Isabel Pinder <isabel.pinder@kinandcarta.com>
2022-05-04 16:24:00 +0100

    create and test List component

---
+++ src/components/general/list/List.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '../../../test-utils';
+import { List } from '.';
+
+test('renders 0 list items when passed an empty array', () => {
+  render(<List items={[]} />);
+
+  const listItems = screen.queryAllByRole('listitem');
+  expect(listItems).toEqual([]);
+});
+
+test('renders 1 list items when passed an array with 1 string element', () => {
+  render(<List items={['test-string']} />);
+
+  const listItems = screen.queryAllByRole('listitem');
+  expect(listItems).toHaveLength(1);
+});
+
+test('renders 3 list items when passed an array with 1 string element', () => {
+  render(<List items={['test-string 1', 'test-string 2', 'test-string 3']} />);
+
+  const listItems = screen.queryAllByRole('listitem');
+  expect(listItems).toHaveLength(3);
+});
