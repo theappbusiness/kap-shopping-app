f2f76b2ade4495e88171d48834c11ce4ff10a929
KStonehouse2001 <khalidstonehouse12@outlook.com>
2022-05-05 17:44:02 +0100

    Added limit to url

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -4,4 +4,4 @@ export const API_GET_PRODUCTS = `${API}/products`;
 export const API_GET_PRODUCT = (id: string): string =>
   `${API_GET_PRODUCTS}/${id}`;
 export const API_SEARCH_PRODUCT = (searchTerm: string) =>
-  `${API_GET_PRODUCTS}?search=${searchTerm}`;
+  `${API_GET_PRODUCTS}?search=${searchTerm}&limit=5`;
