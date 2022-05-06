dbb0b09b360bdb9b58107b796162b7c42b826d32
KStonehouse2001 <khalidstonehouse12@outlook.com>
2022-05-06 16:55:47 +0100

    deleted list component from homepage and rendered it in search component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,17 +1,12 @@
 import { ProductTiles } from '../../components/product-tiles/ProductTiles';
 import { Hero } from '../../components/hero/index';
-import { List } from '../../components/general/list';
 import { SearchInput } from '../../components/search';
 
 export const HomePage: React.FC = () => {
   return (
     <>
       <SearchInput />
-      <List items={[]} />
-      <List items={['test string  1/1']} />
-      <List items={['test string 1/3', 'test string 2/3', 'test string 3/3']} />
       <Hero />
-
       <ProductTiles />
     </>
   );
