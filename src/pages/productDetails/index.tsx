d877d8fb60f266f61fc844f547e89574e847aa05
Isabel Pinder <isabel.pinder@kinandcarta.com>
2022-05-10 11:49:12 +0100

    render ProductDetails component in ProductDetailsPage using a mockProduct as product props

--- src/pages/productDetails/index.tsx
+++ src/pages/productDetails/index.tsx
@@ -1,5 +1,25 @@
 import { ReactElement } from 'react';
+import { ProductDetails } from '../../components/product-details';
+
+const mockProduct = {
+  id: '#1',
+  name: 'Some random product',
+  category: {
+    name: 'General',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
+  },
+  price: 179.9,
+  stock: 5,
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
+};
 
 export const ProductDetailsPage = (): ReactElement => {
-  return <div>Product Details Page</div>;
+  return (
+    <div>
+      Product Details Page
+      <ProductDetails product={mockProduct} />
+    </div>
+  );
 };
