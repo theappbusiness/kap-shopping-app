9bb5c45c673cbc0be3a367814045ae8cc2214642
KStonehouse2001 <khalidstonehouse12@outlook.com>
2022-05-11 10:47:08 +0100

    Refactored test so it mock  calls the API and tests if user types it renders correct item

--- src/components/search/search.test.tsx
+++ src/components/search/search.test.tsx
@@ -1,21 +1,30 @@
+import userEvent from '@testing-library/user-event';
+import { rest } from 'msw';
+import { API_GET_PRODUCTS } from '../../constants/api';
+import { mockProductsResponse } from '../../mocks/mockProductResponse';
+import { server } from '../../mocks/server';
 import { screen, render } from '../../test-utils';
-import { List } from '../general/list';
-
-const mockProducts = [
-  { name: 'Chair', id: 'ID12' },
-  { name: 'Football', id: 'ID13' },
-  { name: 'Sofa', id: 'ID14' },
-];
+import { SearchInput } from '.';
 
 describe('Search', () => {
-  it('renders correct results in a list on screen depending on search input', () => {
-    render(<List items={mockProducts} />);
-    const chairListItem = screen.getByText('Chair');
-    const footballListItem = screen.getByText('Football');
-    const sofaListItem = screen.getByText('Sofa');
-    expect(chairListItem).toBeInTheDocument();
-    expect(footballListItem).toBeInTheDocument();
-    expect(sofaListItem).toBeInTheDocument();
-    expect(screen.getAllByRole('listitem')).toHaveLength(3);
+  it('renders correct results in a list on screen depending on search input', async () => {
+    server.resetHandlers(
+      rest.get(`${API_GET_PRODUCTS}`, (req, res, ctx) => {
+        return res(ctx.status(200), ctx.json(mockProductsResponse));
+      })
+    );
+    render(<SearchInput />);
+    const input = screen.getByTestId('search-input');
+    expect(input).toBeInTheDocument();
+    expect(input).toHaveAttribute('type', 'text');
+
+    await userEvent.type(input, 'Football Everybody');
+    const resultInput = await screen.findByText('Football Everybody');
+    expect(resultInput).toBeInTheDocument();
+
+    userEvent.clear(input);
+    await userEvent.type(input, 'Table');
+    const tableTextEL = await screen.findByText('Table');
+    expect(tableTextEL).toBeInTheDocument();
   });
 });
