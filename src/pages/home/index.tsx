7bb9d3784c9153fc07b7163611bb3b1b6e6bb50a
Isabel Pinder <isabel.pinder@kinandcarta.com>
2022-05-11 16:02:55 +0100

    remove searchInput from homepage

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,9 @@
 import { ProductTiles } from '../../components/product-tiles/ProductTiles';
 import { Hero } from '../../components/hero/index';
-import { SearchInput } from '../../components/search';
 
 export const HomePage: React.FC = () => {
   return (
     <>
-      <SearchInput />
       <Hero />
       <ProductTiles />
     </>
