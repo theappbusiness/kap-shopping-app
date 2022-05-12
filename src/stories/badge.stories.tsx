60e8eba7dd3b8e368789fc8a69ff626139c2423e
KStonehouse2001 <khalidstonehouse12@outlook.com>
2022-05-12 16:39:43 +0100

    Created badge story

---
+++ src/stories/badge.stories.tsx
@@ -0,0 +1,21 @@
+import { ThemeProvider } from 'styled-components';
+import { ComponentStory } from '@storybook/react';
+import { Badge } from '../components/general/badge';
+import { theme } from '../styled';
+
+// eslint-disable-next-line import/no-default-export
+export default {
+  title: 'Badge',
+  component: Badge,
+};
+
+const Template: ComponentStory<typeof Badge> = (args) => (
+  <ThemeProvider theme={theme}>
+    <Badge {...args} />;
+  </ThemeProvider>
+);
+
+export const Standard = Template.bind({});
+Standard.args = {
+  label: '3',
+};
