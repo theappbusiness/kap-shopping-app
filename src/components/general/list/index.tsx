8e8e31af21ada790f96a24d9c588224700b47227
Isabel Pinder <isabel.pinder@kinandcarta.com>
2022-05-12 09:45:15 +0100

    add background-color property

--- src/components/general/list/index.tsx
+++ src/components/general/list/index.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 const StyledListItem = styled.li`
+  background-color: ${({ theme }) => theme.colors.white};
   padding: ${({ theme }) => theme.sizes.spacing.md};
   border-top: -1px solid ${({ theme }) => theme.colors.dark};
   border-bottom: 1px solid ${({ theme }) => theme.colors.dark};
