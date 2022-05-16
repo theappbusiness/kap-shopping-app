3ca290295ae30d7ad7c86b0c259f0d6e45d19ae7
Isabel Pinder <isabel.pinder@kinandcarta.com>
2022-05-16 14:27:19 +0100

    add icons for + -

--- src/registerIcons.ts
+++ src/registerIcons.ts
@@ -5,8 +5,18 @@ import {
   faMagnifyingGlass,
   faShoppingBag,
   faXmark,
+  faPlus,
+  faMinus,
 } from '@fortawesome/free-solid-svg-icons';
 
 export const registerIcons = (): void => {
-  library.add(faCheckSquare, faShoppingBag, faXmark, faMagnifyingGlass, faBars);
+  library.add(
+    faCheckSquare,
+    faShoppingBag,
+    faXmark,
+    faMagnifyingGlass,
+    faBars,
+    faPlus,
+    faMinus
+  );
 };
