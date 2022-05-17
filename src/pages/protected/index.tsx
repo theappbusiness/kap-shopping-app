d16f4c6bedeeb74eaccb1a8948025e59e6f73394
Erica Carvalho <94441307+ericamcarvalho@users.noreply.github.com>
2022-05-17 19:38:14 +0100

    Update index.tsx

--- src/pages/protected/index.tsx
+++ src/pages/protected/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useAuth0, withAuthenticationRequired } from '@auth0/auth0-react';
 import { Spinner } from '../../components/general/Spinner';
 
-export const Protected: React.FC = () => {
+const Protected: React.FC = () => {
   const { user } = useAuth0();
 
   return <div>Protected Page. Hello, {user?.name}!</div>;
