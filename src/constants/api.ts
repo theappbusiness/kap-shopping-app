f3859529b7083c488dea951a55b038fd16c35b9b
Erica Carvalho <94441307+ericamcarvalho@users.noreply.github.com>
2022-05-17 16:59:30 +0100

    Update api.ts

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,4 @@
-const HOST = 'https://d1sq674fk5y5nl.cloudfront.net/';
+const HOST = 'https://d1sq674fk5y5nl.cloudfront.net';
 export const API = `${HOST}/api/v1`;
 export const API_GET_PRODUCTS = `${API}/products`;
 export const API_GET_PRODUCT = (id: string): string =>
