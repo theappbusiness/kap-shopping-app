46fa2bf454374ee4c0ae26fc3c0cd09d79518e87
Erica Carvalho <erica.carvalho@kinandcarta.com>
2022-05-17 18:49:08 +0100

    Fix tests

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
-import { useAuth0 } from '@auth0/auth0-react';
 import { Header } from '../components/header/Header';
 import { Footer } from '../components/footer/Footer';
-import { Spinner } from '../components/general/Spinner';
 
 const Container = styled.div`
   min-height: 100vh;
@@ -11,11 +9,6 @@ const Container = styled.div`
 `;
 
 export const DefaultLayout: React.FC = ({ children }) => {
-  const { isLoading } = useAuth0();
-
-  if (isLoading) {
-    return <Spinner />;
-  }
   return (
     <Container>
       <Header />
--- src/pages/protected/index.tsx
+++ src/pages/protected/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useAuth0, withAuthenticationRequired } from '@auth0/auth0-react';
 import { Spinner } from '../../components/general/Spinner';
 
-const Protected: React.FC = () => {
+export const Protected: React.FC = () => {
   const { user } = useAuth0();
 
   return <div>Protected Page. Hello, {user?.name}!</div>;
