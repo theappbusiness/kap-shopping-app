f7b12e4e608288c06a8619b8b90d2e1b00dcde77
KStonehouse2001 <khalidstonehouse12@outlook.com>
2022-05-18 11:56:44 +0100

    Added cart list items to cart page and added handler function to buttons

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,5 +1,48 @@
-import { ReactElement } from 'react';
+import { ReactElement, useContext } from 'react';
+import { CartItem } from '../../components/general/cart-item';
+import { CartContext } from '../../contexts/Cart';
 
 export const CartPage = (): ReactElement => {
-  return <div>Cart Page</div>;
+  const { cart, setCart } = useContext(CartContext);
+
+  const increaseQuantity = (id: string) => {
+    setCart((currCart) => {
+      return currCart.map((currItem) => {
+        if (currItem.id === id) {
+          return { ...currItem, quantity: currItem.quantity + 1 };
+        }
+        return { ...currItem };
+      });
+    });
+  };
+  const decreaseQuantity = (id: string) => {
+    setCart((currCart) => {
+      return currCart.map((currItem) => {
+        if (currItem.id === id) {
+          return { ...currItem, quantity: currItem.quantity - 1 };
+        }
+        return { ...currItem };
+      });
+    });
+  };
+  const removeItem = (id: string) => {
+    setCart((currCart) => currCart.filter((cartItem) => id !== cartItem.id));
+  };
+  return (
+    <>
+      {cart.map((item) => {
+        return (
+          <CartItem
+            key={item.id}
+            imgSrc="https://picsum.photos/seed/test/270"
+            name={item.name}
+            quantity={item.quantity}
+            onDecreaseStepperClick={() => decreaseQuantity(item.id)}
+            onIncreaseStepperClick={() => increaseQuantity(item.id)}
+            onRemoveClick={() => removeItem(item.id)}
+          />
+        );
+      })}
+    </>
+  );
 };
