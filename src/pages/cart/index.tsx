5f5e0b9c7b7d2c30b3524422c6b44fe6d03b49ad
KStonehouse2001 <khalidstonehouse12@outlook.com>
2022-05-18 16:27:00 +0100

    removed cart list item functionality from cart page file

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,42 +1,10 @@
-import { ReactElement, useContext } from 'react';
-import { CartItem } from '../../components/general/cart-item';
-import { CartContext } from '../../contexts/Cart';
+import { ReactElement } from 'react';
 
 export const CartPage = (): ReactElement => {
-  const { cart, setCart } = useContext(CartContext);
-  type Amount = 'increment' | 'decrement';
-  const changeQuantity = (id: string, amount: Amount) => {
-    setCart((currCart) => {
-      return currCart.map((currItem) => {
-        if (currItem.id === id) {
-          if (amount === 'increment')
-            return { ...currItem, quantity: currItem.quantity + 1 };
-          if (amount === 'decrement')
-            return { ...currItem, quantity: currItem.quantity - 1 };
-        }
-        return { ...currItem };
-      });
-    });
-  };
-  const removeItem = (id: string) => {
-    setCart((currCart) => currCart.filter((cartItem) => id !== cartItem.id));
-  };
+
   return (
     <>
       <h3>Cart Page</h3>
-      {cart.map((item) => {
-        return (
-          <CartItem
-            key={item.id}
-            imgSrc="https://picsum.photos/seed/test/270"
-            name={item.name}
-            quantity={item.quantity}
-            onDecreaseStepperClick={() => changeQuantity(item.id, 'decrement')}
-            onIncreaseStepperClick={() => changeQuantity(item.id, 'increment')}
-            onRemoveClick={() => removeItem(item.id)}
-          />
-        );
-      })}
     </>
   );
 };
