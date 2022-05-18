8ac5aaee7555ed9156f08c338f125176b307d4cf
KStonehouse2001 <khalidstonehouse12@outlook.com>
2022-05-18 16:28:02 +0100

    Added cart list item button functionality to CartList component

--- src/components/cart-list/index.tsx
+++ src/components/cart-list/index.tsx
@@ -4,7 +4,24 @@ import { List } from '../general/list';
 import { CartContext } from '../../contexts/Cart';
 
 export const CartList: React.FC = () => {
-  const { cart } = useContext(CartContext);
+  const { cart, setCart } = useContext(CartContext);
+  const changeQuantity = (id: string, amount: number, sign: string) => {
+    setCart((currCart) => {
+      return currCart.map((currItem) => {
+        if (currItem.id === id) {
+          if (sign === '+') {
+            return { ...currItem, quantity: currItem.quantity + amount };
+          } else if (sign === '-' && currItem.quantity > 0) {
+            return { ...currItem, quantity: currItem.quantity - amount };
+          }
+        }
+        return { ...currItem };
+      });
+    });
+  };
+  const removeItem = (id: string) => {
+    setCart((currCart) => currCart.filter((cartItem) => id !== cartItem.id));
+  };
 
   const cartItems: React.ReactElement[] = cart.map((item) => {
     return (
@@ -13,9 +30,9 @@ export const CartList: React.FC = () => {
         imgSrc={`https://picsum.photos/seed/${item.name}/270`}
         name={item.name}
         quantity={item.quantity}
-        onDecreaseStepperClick={() => undefined}
-        onIncreaseStepperClick={() => undefined}
-        onRemoveClick={() => undefined}
+        onDecreaseStepperClick={() => changeQuantity(item.id, 1, '-')}
+        onIncreaseStepperClick={() => changeQuantity(item.id, 1, '+')}
+        onRemoveClick={() => removeItem(item.id)}
       />
     );
   });
