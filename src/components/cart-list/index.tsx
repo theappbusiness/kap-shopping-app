146594ab08fa056dcfc052afed15fcca2a9fcd65
Isabel Pinder <isabel.pinder@kinandcarta.com>
2022-05-19 12:19:04 +0100

    refactor CartList to use cart functions from cart context

--- src/components/cart-list/index.tsx
+++ src/components/cart-list/index.tsx
@@ -2,10 +2,9 @@ import { useContext } from 'react';
 import { CartItem } from '../general/cart-item';
 import { List } from '../general/list';
 import { CartContext } from '../../contexts/Cart';
-import { changeQuantity, removeItem } from '../../utils/cartModifiers';
 
 export const CartList: React.FC = () => {
-  const { cart, setCart } = useContext(CartContext);
+  const { cart, changeQuantity, removeItem } = useContext(CartContext);
 
   const cartItems: React.ReactElement[] = cart.map((item) => {
     return (
@@ -14,9 +13,9 @@ export const CartList: React.FC = () => {
         imgSrc={`https://picsum.photos/seed/${item.name}/270`}
         name={item.name}
         quantity={item.quantity}
-        onDecreaseStepperClick={() => changeQuantity(item.id, -1, setCart)}
-        onIncreaseStepperClick={() => changeQuantity(item.id, 1, setCart)}
-        onRemoveClick={() => removeItem(item.id, setCart)}
+        onDecreaseStepperClick={() => changeQuantity(item.id, -1)}
+        onIncreaseStepperClick={() => changeQuantity(item.id, 1)}
+        onRemoveClick={() => removeItem(item.id)}
       />
     );
   });
