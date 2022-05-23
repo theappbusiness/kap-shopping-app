73a71f0fa41494f444c1c1cc540f027348203785
Isabel Pinder <isabel.pinder@kinandcarta.com>
2022-05-23 17:32:28 +0100

    create 30 days variable for readability

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -1,6 +1,7 @@
 export const createCartCookie = (cartId: string): void => {
   const date = new Date();
-  date.setTime(date.getTime() + 30 * 24 * 60 * 60 * 1000);
+  const thirtyDays = 30 * 24 * 60 * 60 * 1000;
+  date.setTime(date.getTime() + thirtyDays);
 
   document.cookie = `cartId=${cartId}; expires=${date.toUTCString()};`;
 };
